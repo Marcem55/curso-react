55cbe22186f3ab83eb238faf9e912715f0a7b6f1
Marcem55 <malacalzamarcelo@gmail.com>
2023-07-18 18:24:50 -0300

    pointer move ready

--- projects/02-mouse-follower/src/App.jsx
+++ projects/02-mouse-follower/src/App.jsx
@@ -1,10 +1,58 @@
+import { useEffect, useState } from "react"
 
-function App() {
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('useEffect')
+
+    const handleMove = (e) => {
+      const { clientX, clientY } = e
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  useEffect(() => {
+    document.body.classList.toggle('pointer-enabled', enabled)
+
+    return () => {
+      document.body.classList.remove('pointer-enabled')
+    }
+  }, [enabled])
 
   return (
-    <h3>
-      Proyecto 3
-    </h3>
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#ded',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: enabled ? `translate(${position.x}px, ${position.y}px)` : '',
+      }}></div>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
